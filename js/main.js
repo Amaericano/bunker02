e84f1f64b65f53400124498790871dcdc982f263
Amaericano <61690125+Amaericano@users.noreply.github.com>
2020-07-08 22:48:38 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function () {
+  $(".fairy-tail__slider, .our-trip__slider").slick({
+    prevArrow:
+      '<button type="button" class="slick-btn	slick-prev"><img src="img/back.svg" alt=""></button>',
+    nextArrow:
+      '<button type="button" class="slick-btn	slick-next"><img src="img/next.svg" alt=""></button>',
+    autoplay: true,
+    fade: true,
+  });
+});
